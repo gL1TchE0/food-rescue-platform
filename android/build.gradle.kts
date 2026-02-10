2bf7888c60c00b8e5701bff744af518f4fb3d569
AKSHAY30080605 <akshayrtrichy@gmail.com0>
2026-02-10 22:54:14 +0530

    Update AndroidManifest and build.gradle.kts to enable cleartext traffic and set compileSdkVersion

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,6 +19,28 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+subprojects {
+    val configureAndroid = {
+        project.extensions.findByName("android")?.let {
+            val android = it as com.android.build.gradle.BaseExtension
+            if (android.compileSdkVersion == null) {
+                android.compileSdkVersion(34)
+            }
+            if (android.namespace == null) {
+                android.namespace = "com.example.${project.name}"
+            }
+        }
+    }
+    
+    if (project.state.executed) {
+        configureAndroid()
+    } else {
+        project.afterEvaluate {
+            configureAndroid()
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
